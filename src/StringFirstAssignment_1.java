56102b0ea52ca272a41c0ca0e57706e3aa2d92dd
AnmolMajithia <rush2asm@gmail.com>
2020-05-09 13:10:20 +0530

    Strings

--- src/StringFirstAssignment_1.java
+++ src/StringFirstAssignment_1.java
@@ -1,3 +1,35 @@
 public class StringFirstAssignment_1 {
+    private String findSimpleGene(String dna) {
+        int startCodon = dna.indexOf("ATG");
+        if(startCodon == -1) {
+            return "";
+        }
+        int stopCodon = dna.indexOf("TAA",startCodon+1);
+        if(stopCodon == -1) {
+            return "";
+        }
+        if((stopCodon - startCodon) % 3 == 0) {
+            return dna.substring(startCodon, stopCodon+3);
+        } else {
+            return "";
+        }
+    }
 
+    private void testSimpleGene() {
+        String dna = "AGCTATCGATAACG";
+        System.out.println("DNA : " + dna + "\nGene : " + findSimpleGene(dna));
+        dna = "ATGAACGAAC";
+        System.out.println("DNA : " + dna + "\nGene : " + findSimpleGene(dna));
+        dna = "ACACGCATCGAC";
+        System.out.println("DNA : " + dna + "\nGene : " + findSimpleGene(dna));
+        dna = "ATGAGCTAA";
+        System.out.println("DNA : " + dna + "\nGene : " + findSimpleGene(dna));
+        dna = "ATGACTAA";
+        System.out.println("DNA : " + dna + "\nGene : " + findSimpleGene(dna));
+    }
+
+    public static void main(String[] args) {
+        StringFirstAssignment_1 ob = new StringFirstAssignment_1();
+        ob.testSimpleGene();
+    }
 }
